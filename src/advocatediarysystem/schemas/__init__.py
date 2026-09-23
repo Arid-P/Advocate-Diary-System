6412026928cd0cc8b2e01d9cf6c37d0cf1a8439b
Arid-P <aridamanpatel.srijan@gmail.com>
2026-09-23 23:42:01 +0530

    feat(schemas): export Advocate schemas from schemas module

--- src/advocatediarysystem/schemas/__init__.py
+++ src/advocatediarysystem/schemas/__init__.py
@@ -1,10 +1,11 @@
 from .client import ClientBase, ClientCreate, ClientUpdate, ClientRead
-from .case  import CaseBase, CaseCreate, CaseUpdate, CaseRead
+from .case import CaseBase, CaseCreate, CaseUpdate, CaseRead
 from .hearing import HearingBase, HearingCreate, HearingUpdate, HearingRead
+from .advocate import AdvocateBase, AdvocateCreate, AdvocateLogin, AdvocateRead
 
 __all__ = [
     "ClientBase", "ClientCreate", "ClientUpdate", "ClientRead", 
     "CaseBase", "CaseCreate", "CaseUpdate", "CaseRead", 
-    "HearingBase","HearingCreate","HearingUpdate","HearingRead"
-
+    "HearingBase", "HearingCreate", "HearingUpdate", "HearingRead",
+    "AdvocateBase", "AdvocateCreate", "AdvocateLogin", "AdvocateRead",
 ]
