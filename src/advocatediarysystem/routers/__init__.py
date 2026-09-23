2cf1384d9e30a3e4d79e667b65083a291e5b27d2
Arid-P <aridamanpatel.srijan@gmail.com>
2026-09-23 23:43:32 +0530

    feat(api): export auth_router from routers module

--- src/advocatediarysystem/routers/__init__.py
+++ src/advocatediarysystem/routers/__init__.py
@@ -1,3 +1,6 @@
 from .clients import router as clients_router
 from .cases import router as cases_router
 from .hearings import router as hearings_router
+from .auth import router as auth_router
+
+__all__ = ["clients_router", "cases_router", "hearings_router", "auth_router"]
