31bb0d9d9b5b341da3d901b9c427d2ae09b4e452
Arid-P <aridamanpatel.srijan@gmail.com>
2026-09-23 23:41:16 +0530

    feat(models): export Advocate model from models module

--- src/advocatediarysystem/models/__init__.py
+++ src/advocatediarysystem/models/__init__.py
@@ -1,5 +1,6 @@
 from .client import Client
 from .case import Case
 from .hearing import Hearing
+from .advocate import Advocate
 
-__all__ = ["Client", "Case", "Hearing"]
+__all__ = ["Client", "Case", "Hearing", "Advocate"]
