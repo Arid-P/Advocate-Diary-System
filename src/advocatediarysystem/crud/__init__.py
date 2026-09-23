03e50246ad1abecb22dc890c7e97dfdc74493b01
Arid-P <aridamanpatel.srijan@gmail.com>
2026-09-23 23:15:54 +0530

    feat(crud): export get_client_by_phone from crud module

--- src/advocatediarysystem/crud/__init__.py
+++ src/advocatediarysystem/crud/__init__.py
@@ -3,6 +3,7 @@
 from .client import (
     create_client,
     get_client_by_id,
+    get_client_by_phone,
     get_clients,
     update_client,
     delete_client,
@@ -25,7 +26,7 @@ from .hearing import (
 )
 
 __all__ = [
-    "create_client", "get_client_by_id", "get_clients", "update_client", "delete_client",
+    "create_client", "get_client_by_id", "get_client_by_phone", "get_clients", "update_client", "delete_client",
     "create_case", "get_case_by_id", "get_cases", "get_cases_by_client", "update_case", "delete_case",
     "create_hearing", "get_hearing_by_id", "get_hearings", "get_hearings_by_case", "update_hearing", "delete_hearing",
 ]
